eb81d6b509cbc5f549423ea7fedc70b3220de810
Kosuke Kusano <cocuh.kk@gmail.com>
2017-02-25 18:20:39 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='asagami',
+    version='0.0.1',
+    packages=['asagami', 'asagami.modules'],
+    url='',
+    license='MIT',
+    author='cocuh',
+    author_email='cocuh.kk@gmail.com',
+    description=''
+)
